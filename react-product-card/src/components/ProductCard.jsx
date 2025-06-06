21837cde601aa032321d58771a509644d0770493
Morad17 <morad123@hotmail.co.uk>
2025-06-06 19:14:15 +0100

    finished initial mobile styling

--- react-product-card/src/components/ProductCard.jsx
+++ react-product-card/src/components/ProductCard.jsx
@@ -1,39 +1,48 @@
-import React from 'react'
-import ProductImage from '../assets/images/image-product-desktop.jpg'
-import MobileProductImage from '../assets/images/image-product-mobile.jpg'
-import cartIcon from '../assets/images/icon-cart.svg'
+import React from "react";
+import ProductImage from "../assets/images/image-product-desktop.jpg";
+import MobileProductImage from "../assets/images/image-product-mobile.jpg";
+import cartIcon from "../assets/images/icon-cart.svg";
 
 const ProductCard = () => {
+    // // Check if the screen width is less than or equal to 768px
+    // const isMobile = window.innerWidth <= 768;
+    // // Use the mobile image if on a mobile device, otherwise use the desktop image
+    // const responsiveImg = isMobile ? MobileProductImage : ProductImage;  
+
   return (
     <div className="product-card">
         <div className="product-card-image">
+        <picture>
+            <source srcSet={MobileProductImage} media="(max-width: 425px)" />
             <img src={ProductImage} alt="Product" />
+        </picture>
         </div>
-        <div className="product-card-info">
-            <div className="product-card-wrapper">
-                <h3 className="product-card-category">PERFUME</h3>
-                <div className="product-card-name-main">
-                                <h1 className="product-card-name">Gabrielle</h1>
-            <h1 className="product-card-name">Esscence Eau</h1>
-            <h1 className="product-card-name">De Parfum</h1>
-                </div>
+      <div className="product-card-info">
+        <div className="product-card-wrapper">
+          <h3 className="product-card-category">PERFUME</h3>
+          <div className="product-card-name-main">
+            <h1 className="product-card-name">Gabrielle
+            Esscence Eau
+            De Parfum</h1>
+          </div>
 
-
-            <p className="product-card-description">
-                A floral, solar and voluptuous interpretation composed by Olivier Polge, 
-                Perfume-Creater for the House of CHANEL.
-            </p>
-            <div className="product-card-price">
-                <span className="product-card-price-current">$149.99</span>
-                <span className="product-card-price-original"><s>$169.99</s> </span>
-            </div>
-                <button className="product-card-btn">
-                    <img className="svg"src={cartIcon} alt="" /> Add to Cart
-                </button> 
-            </div>
+          <p className="product-card-description">
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfume-Creater for the House of CHANEL.
+          </p>
+          <div className="product-card-price">
+            <span className="product-card-price-current">$149.99</span>
+            <span className="product-card-price-original">
+              <s>$169.99</s>{" "}
+            </span>
+          </div>
+          <button className="product-card-btn">
+            <img className="svg" src={cartIcon} alt="" /> Add to Cart
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
